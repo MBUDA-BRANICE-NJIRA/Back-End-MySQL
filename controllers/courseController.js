1116223b2261422e7cccfd731a67c736b455840f
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-20 12:56:42 +0300

    Started working on the course controller

---
+++ controllers/courseController.js
@@ -0,0 +1,74 @@
+const db = require("../models/indexStart");
+const createError = require("http-errors");
+
+//use the model
+const Course = db.students;
+// const Course = db.courses
+
+module.exports = {
+  //Add student
+  addCourse: async (req, res, next) => {
+    try {
+      let info = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        gender: req.body.gender,
+        //course_id: req.body.course_id
+      };
+
+      const addStudent = await Student.create(info);
+      res.status(200).send(addStudent);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //get all students with course
+  getStudents: async (req, res, next) => {
+    try {
+      let allStudents = await Student.findAll({});
+      res.status(200).send(allStudents);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //get student by id
+  getStudent: async (req, res, next) => {
+    try {
+      let id = req.params.id;
+      let student = await Student.findOne({ where: { student_id: id } });
+      if (!student) {
+        throw createError(404, "Student does not exist");
+      }
+      res.status(200).send(student);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //Update
+  UpdateStudent: async (req, res, next) => {
+    try {
+      let id = req.params.id;
+      const student = await Student.findOne({ where: { student_id: id } });
+      if (!student) {
+        throw createError(404, "Student does not exist");
+      }
+      res.status(202).send(student);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //Delete
+  deleteStudent: async (req, res, next) => {
+    try{
+      let id = req.params.id;
+      await Student.destroy({where: {student_id: id}})
+      res.status(202).send("Student deleted succesfuly")
+    }catch(error){
+      next(error)
+    }
+  }
+};
--- models/courseModel.js
+++ models/courseModel.js
@@ -1,21 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
+  const course = sequelize.define("Course", {
+    course_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-    const course = sequelize.define("Course", {
-      course_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-  
-      Coursename: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      
-     
-     
-    });
-  
-    return course;
-  };
-  
+    Coursename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return course;
+};
