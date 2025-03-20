0721f84c6869d03e3dda7568f7eb94c3a8085e88
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-20 12:39:26 +0300

    Worked on the course model

---
+++ models/courseModel.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const course = sequelize.define("Course", {
+      course_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+  
+      Coursename: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      
+     
+     
+    });
+  
+    return course;
+  };
+  
