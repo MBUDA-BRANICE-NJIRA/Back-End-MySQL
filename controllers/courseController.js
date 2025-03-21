2778aeb84236df58d381e08f9a191b3cc00ddb36
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-21 11:27:25 +0300

    When testing realized i was having an issue started deburging

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,15 +1,17 @@
 const db = require("../models/indexStart");
 const createError = require("http-errors");
 
-// use the model
-const Course = db.models.Course; // <--- FIX: use db.models.Course instead of db.courses
+//use the model
+const Course = db.course;
+
 
 module.exports = {
-  // Add Course
+  //Add Course
   addCourse: async (req, res, next) => {
     try {
       let info = {
         coursename: req.body.coursename,
+       
       };
 
       const addCourse = await Course.create(info);
@@ -19,7 +21,7 @@ module.exports = {
     }
   },
 
-  // Get all courses
+  //get all  course
   getCourse: async (req, res, next) => {
     try {
       let allCourses = await Course.findAll({});
@@ -29,8 +31,8 @@ module.exports = {
     }
   },
 
-  // Get Course by id
-  getCourseById: async (req, res, next) => { // <--- FIX: rename function to avoid duplicate name
+  //get Course by id
+  getCourse: async (req, res, next) => {
     try {
       let id = req.params.id;
       let course = await Course.findOne({ where: { course_id: id } });
@@ -43,29 +45,28 @@ module.exports = {
     }
   },
 
-  // Update course
-  updateCourse: async (req, res, next) => { // <--- FIX: rename function to avoid duplicate name
+  //Update course
+  UpdateCourse: async (req, res, next) => {
     try {
       let id = req.params.id;
       const course = await Course.findOne({ where: { course_id: id } });
       if (!course) {
         throw createError(404, "Course does not exist");
       }
-      // update course logic here
       res.status(202).send(course);
     } catch (error) {
       next(error);
     }
   },
 
-  // Delete Course
+  //Delete Course
   deleteCourse: async (req, res, next) => {
-    try {
+    try{
       let id = req.params.id;
-      await Course.destroy({ where: { course_id: id } });
-      res.status(202).send("Course deleted successfully");
-    } catch (error) {
-      next(error);
+      await Course.destroy({where: {course_id: id}})
+      res.status(202).send("Course deleted succesfuly")
+    }catch(error){
+      next(error)
     }
-  },
+  }
 };
--- models/courseModel.js
+++ models/courseModel.js
@@ -1,16 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
-  const course = sequelize.define("Course", {
+  const Course = sequelize.define("Course", {
     course_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-
-    Coursename: {
+    coursename: { 
       type: DataTypes.STRING,
       allowNull: false,
     },
   });
 
-  return course;
+  return Course;
 };
