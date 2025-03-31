bcac99c44ea59fc7e611234b70d943e980d0c193
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-31 17:47:38 +0300

    Formated the indexstart document

--- models/indexStart.js
+++ models/indexStart.js
@@ -1,45 +1,39 @@
-const dbConfig = require('../config/dbConfig')
+const dbConfig = require("../config/dbConfig");
 
-const {Sequelize, DataTypes} = require('sequelize')//Sequalize is like mongoose 
+const { Sequelize, DataTypes } = require("sequelize"); //Sequalize is like mongoose
 
 //We create an object from sequilize
 
-
-
-const sequelize = new Sequelize(
-    dbConfig.DB,
-    dbConfig.USER,
-    dbConfig.PASSWORD,
-    {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        operatorsAliases: false //If errors in your code will overwrite the errors using this line
-    }
-);
-
-sequelize.authenticate()
-.then(() => {
-    console.log('Database connection Succesfull ...')
-})
-.catch(err => {
-    console.log('Error' + err);
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false, //If errors in your code will overwrite the errors using this line
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection Succesfull ...");
+  })
+  .catch((err) => {
+    console.log("Error" + err);
+  });
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.students = require('./studentModel.js')(sequelize, DataTypes);//This the part connectin us to the model
-db.courses = require('./courseModel.js')(sequelize, DataTypes);//This the part connecting us to the model
+db.students = require("./studentModel.js")(sequelize, DataTypes); //This the part connectin us to the model
+db.courses = require("./courseModel.js")(sequelize, DataTypes); //This the part connecting us to the model
 
-
-db.sequelize.sync ({force: false})//If u set to true its going to refresh and this clears all data in the database
-.then(() => {
-    console.log('re-sync done');
-});
+db.sequelize
+  .sync({ force: false }) //If u set to true its going to refresh and this clears all data in the database
+  .then(() => {
+    console.log("re-sync done");
+  });
 
 //This the section responsible for creating the foreign null section in the database
-db.courses.hasOne(db.students, { foreignKey: 'course_id' });
-db.students.belongsTo(db.courses, { foreignKey: 'course_id' });
+db.courses.hasOne(db.students, { foreignKey: "course_id" });
+db.students.belongsTo(db.courses, { foreignKey: "course_id" });
 
-module.exports = db
+module.exports = db;
